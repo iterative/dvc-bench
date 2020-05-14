729b3166e7a82d6feac24c3e4a771b03c9ab141c
Paweł Redzyński <pawelredzynski@gmail.com>
2020-05-14 18:47:29 +0200

    setup initial workflow

---
+++ tags_to_sha.py
@@ -0,0 +1,18 @@
+import os
+
+from git import Repo
+
+
+def convert_to_sha(tags_filename="tags.txt", hashes_filename="hashes.txt"):
+    tags = []
+    with open(tags_filename, "r") as fobj:
+        tags.extend([l.strip() for l in fobj.readlines()])
+
+    git_repo = Repo("dvc")
+    hashes = [git_repo.commit(t).hexsha + os.linesep for t in tags]
+
+    with open(hashes_filename, "w") as fobj:
+        fobj.writelines(hashes)
+
+
+convert_to_sha()

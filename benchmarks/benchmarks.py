d4c4bab9e7941fde469d068a64a4bfdce5a6aec2
Paweł Redzyński <pawelredzynski@gmail.com>
2020-05-18 20:36:29 +0200

    benchmark: add aita, cache type copy

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,7 +1,11 @@
-import os
-
 from dvc.main import main
-from tempfile import TemporaryDirectory
+from tempfile import TemporaryDirectory, gettempdir
+import os
+from git import Repo as GitRepo
+from dvc.repo import Repo as DvcRepo
+import shutil
+
+DATA_SOURCES_DIR = os.path.join(gettempdir(), "data_sources")
 
 
 class BaseBench:
@@ -23,10 +27,41 @@ class InitNoScmBench(BaseBench):
 class InitScmBench(BaseBench):
     def setup(self):
         super().setup()
-        from git import Repo
 
-        git = Repo.init(self.test_directory.name)
+        git = GitRepo.init(self.test_directory.name)
         git.close()
 
     def time_init(self):
         main(["init"])
+
+
+class AITABench(BaseBench):
+    def _download_resources(self):
+        from dvc.repo import Repo
+
+        os.makedirs(DATA_SOURCES_DIR, exist_ok=True)
+
+        self.dataset_path = os.path.join(DATA_SOURCES_DIR, "aita.csv")
+        if not os.path.exists(self.dataset_path):
+            Repo.get(
+                "https://github.com/iterative/aita_dataset",
+                "aita_clean.csv",
+                out=self.dataset_path,
+                rev="lightweight",
+            )
+
+    def setup(self):
+        super().setup()
+        self._download_resources()
+
+        git = GitRepo.init(self.test_directory.name)
+        git.close()
+
+        self.repo = DvcRepo.init(self.test_directory.name)
+
+        self.data_path = os.path.join(self.test_directory.name, "aita.csv")
+
+        shutil.copy(self.dataset_path, self.data_path)
+
+    def time_add_copy(self):
+        self.repo.add([self.data_path])

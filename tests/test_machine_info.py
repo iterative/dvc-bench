dbe5d361d0071a5a045b34bd173c49efcc9892aa
Paweł Redzyński <pawelredzynski@gmail.com>
2020-05-29 19:09:24 +0200

    asv machine: generate custom machine info

---
+++ tests/test_machine_info.py
@@ -0,0 +1,49 @@
+import hashlib
+import json
+
+import pytest
+from asv.machine import Machine, MachineCollection
+
+from write_asv_machine import floor_nearest, write_machine_info
+
+
+@pytest.mark.parametrize("ram, expected_ram", [("98765", "98700"), ("", "")])
+def test_write_asv_machine(mocker, ram, expected_ram):
+    mocker.patch.object(
+        Machine,
+        "get_defaults",
+        return_value={
+            "ram": ram,
+            "machine": "machine_name",
+            "other_info": "other_info",
+        },
+    )
+
+    expected_name = "machine_{}".format(
+        hashlib.md5(
+            json.dumps(
+                {"ram": expected_ram, "other_info": "other_info"},
+                sort_keys=True,
+            ).encode("utf-8")
+        ).hexdigest()[:5]
+    )
+
+    save_mock = mocker.patch.object(MachineCollection, "save")
+    write_machine_info()
+
+    assert save_mock.call_args == mocker.call(
+        expected_name,
+        {
+            "ram": expected_ram,
+            "machine": expected_name,
+            "other_info": "other_info",
+        },
+    )
+
+
+@pytest.mark.parametrize(
+    "x, floor, expected_result",
+    [(199, 100, 100), (101, 100, 100), (99, 100, 0)],
+)
+def test_floor_nearest(x, floor, expected_result):
+    assert floor_nearest(x, floor) == expected_result
---
+++ write_asv_machine.py
@@ -0,0 +1,37 @@
+import copy
+import hashlib
+import json
+import os
+
+from asv.machine import Machine, MachineCollection
+
+
+def floor_nearest(x, floor):
+    return x // floor * floor
+
+
+def write_machine_info():
+    # Hack making sure that similar machines will have same name, so that
+    # we can reuse results from previous runs.
+    if os.path.exists(MachineCollection.get_machine_file_path()):
+        os.remove(MachineCollection.get_machine_file_path())
+
+    machine_info = Machine.get_defaults()
+    # floor to tolerate small fluctuations in RAM between runners
+    if machine_info["ram"]:
+        machine_info["ram"] = str(floor_nearest(int(machine_info["ram"]), 100))
+
+    machine_marker = copy.copy(machine_info)
+    del machine_marker["machine"]
+
+    marker_str = json.dumps(machine_marker, sort_keys=True)
+    suffix = hashlib.md5(marker_str.encode("utf-8")).hexdigest()[:5]
+
+    machine_name = "machine_{}".format(suffix)
+    machine_info["machine"] = machine_name
+
+    MachineCollection.save(machine_name, machine_info)
+
+
+if __name__ == "__main__":
+    write_machine_info()

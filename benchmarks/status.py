6525ac4978bbc95fce788e585ec5c99041000838
karajan1001 <mishanyo1001@gmail.com>
2020-05-31 14:17:38 +0800

    `dvc add` before copy files, reduce runtime

--- benchmarks/status.py
+++ benchmarks/status.py
@@ -6,7 +6,7 @@ from dvc.ignore import DvcIgnore
 from dvc.main import main
 
 
-class DVCIgnoreEmpty(BaseBench):
+class DVCIgnore(BaseBench):
     def add_ignore_rules(self, number):
         with open(
             os.path.join(self.test_directory.name, DvcIgnore.DVCIGNORE_FILE),
@@ -19,21 +19,21 @@ class DVCIgnoreEmpty(BaseBench):
         super().setup()
         self.repo = init_dvc(self.test_directory.name)
         dataset_path = random_data_dir(10000, 10)
-        shutil.copytree(dataset_path, "data")
-        assert main(["config", "cache.type", "symlink"]) == 0
-        assert main(["add", "data"]) == 0
+        os.makedirs(os.path.join(self.test_directory.name, 'data'), exist_ok=True)
+        assert main(["add", "data", "--quiet"]) == 0
+        shutil.copytree(dataset_path, "data/data")
+        # calculating md5
+        assert main(["status", "--quiet"]) == 1
 
-    def time_status(self):
-        assert main(["status", "--quiet"]) == 0
+    def time_status_empty(self):
+        self.add_ignore_rules(0)
+        assert main(["status", "--quiet"]) == 1
 
-
-class DVCIgnore3Rules(DVCIgnoreEmpty):
-    def setup(self):
-        super().setup()
+    def time_status_3_rules(self):
         self.add_ignore_rules(3)
+        assert main(["status", "--quiet"]) == 1
 
-
-class DVCIgnore20Rules(DVCIgnoreEmpty):
-    def setup(self):
-        super().setup()
+    def time_status_20_rules(self):
         self.add_ignore_rules(20)
+        assert main(["status", "--quiet"]) == 1
+

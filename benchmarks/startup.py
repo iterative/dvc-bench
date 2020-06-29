146c9f5431972ea8edeb2ecc2256d3de1fd1f33e
Paweł Redzyński <pawelredzynski@gmail.com>
2020-06-29 18:59:58 +0200

    startup: increase bench runs (#121)

--- benchmarks/startup.py
+++ benchmarks/startup.py
@@ -4,6 +4,9 @@ from benchmarks.base import BaseBench
 
 
 class StartupBench(BaseBench):
+    number = 50
+    repeat = (50, 100, 120.0)
+
     def time_startup(self):
         proc = Popen(["dvc", "--help"], stdout=PIPE)
         proc.communicate()

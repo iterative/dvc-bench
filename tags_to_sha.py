afae1e660462f7fe9dd3ba2d2d92bf10de5282a5
Paweł Redzyński <pawelredzynski@gmail.com>
2020-07-29 08:43:28 +0200

    pipeline: fix repo handling (#190)

--- tags_to_sha.py
+++ tags_to_sha.py
@@ -1,6 +1,15 @@
 import os
+from contextlib import contextmanager
 
+from asv.config import Config
 from git import Repo
+
+
+@contextmanager
+def dvc_git_repo():
+    repo = Repo(Config.load("asv.conf.json").project)
+    yield repo
+    repo.close()
 
 
 def convert_to_sha(tags_filename="tags.txt", hashes_filename="hashes.txt"):
@@ -8,8 +17,8 @@ def convert_to_sha(tags_filename="tags.txt", hashes_filename="hashes.txt"):
     with open(tags_filename, "r") as fobj:
         tags.extend([l.strip() for l in fobj.readlines()])
 
-    git_repo = Repo("dvc")
-    hashes = [git_repo.commit(t).hexsha + os.linesep for t in tags]
+    with dvc_git_repo() as repo:
+        hashes = [repo.commit(t).hexsha + os.linesep for t in tags]
 
     with open(hashes_filename, "w") as fobj:
         fobj.writelines(hashes)

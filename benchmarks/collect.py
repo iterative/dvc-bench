cbf41404b523a0eac4bc282cf3d8c4bac88549f5
Paweł Redzyński <pawelredzynski@gmail.com>
2021-01-27 12:56:25 +0100

    repo: benchmark brancher

--- benchmarks/collect.py
+++ benchmarks/collect.py
@@ -1,4 +1,5 @@
 from benchmarks.base import BaseBench
+from dvc.repo import Repo
 
 
 class CollectBench(BaseBench):
@@ -17,3 +18,24 @@ class CollectBench(BaseBench):
 
     def time_stages_collection(self):
         self.dvc("status", "--quiet", proc=True)
+
+
+class TraverseGitRepoBench(BaseBench):
+    repeat = 1
+    number = 3
+    timeout = 1200
+
+    def clone(self, url, tag):
+        # clone only branch containing the tag
+        self.proc("git", "clone", "--branch", tag, "--single-branch", url, ".")
+
+    def setup(self):
+        super().setup()
+        self.clone("https://github.com/iterative/dvc.git", tag="1.10.0")
+
+        # 6024 commits + workspace
+        assert len(list(Repo().brancher(all_commits=True))) == 6025
+
+    def time_repo_traversing(self):
+        # DVC has no metrics, hence return_code==1
+        self.dvc("metrics", "show", "--all-commits", return_code=1, proc=True)

b480b9359e04038c2d203d6a55836205bec5a711
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-05-10 11:35:39 +0300

    benchs: add pull missing benchmark

--- benchmarks/pull.py
+++ benchmarks/pull.py
@@ -1,16 +1,43 @@
+import os
+import shutil
+
 from benchmarks.base import BaseRemoteBench
 
 
-class DVCPullBench(BaseRemoteBench):
+class PullBench(BaseRemoteBench):
     repeat = 1
     timeout = 12000
 
     def setup(self):
         super().setup()
 
-        self.setup_data("cats_dogs", name="data", fs="local")
+        self.gen("data", "cats_dogs")
         self.dvc("add", "data", "--quiet")
         self.dvc("push", "data", "--quiet")
 
+        shutil.rmtree("data")
+        shutil.rmtree(os.path.join(".dvc", "cache"))
+
     def time_cats_dogs(self):
+        self.dvc("pull", "-j", "2", proc=True)
+
+
+class PullOnlyMissingBench(BaseRemoteBench):
+
+    repeat = 1
+    timeout = 12000
+
+    def setup(self):
+        super().setup()
+
+        self.gen("data", "large")
+        self.dvc("add", "data", "--quiet")
+        self.dvc("push", "data", "--quiet")
+
+        shutil.rmtree("data")
+        shutil.rmtree(os.path.join(".dvc", "cache"))
+
+        self.gen("data", "medium")
+
+    def time_pull_missing(self):
         self.dvc("pull", "-j", "2", proc=True)

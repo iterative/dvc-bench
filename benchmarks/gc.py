a11081e88a9dc9d1a7d4184d60c41086d170b9e5
Paweł Redzyński <pawelredzynski@gmail.com>
2021-06-16 22:11:04 +0200

    gc: fix benchmarks

--- benchmarks/gc.py
+++ benchmarks/gc.py
@@ -13,11 +13,9 @@ class GCBench(BaseBench):
 
         self.gen("data", "cats_dogs")
         self.dvc("add", "data", "--quiet")
-        self.dvc("push", "data", "--quiet")
 
         # remove everything from the local system
         self.dvc("remove", "data.dvc")
-        self.dvc("gc", "-w")
 
     def time_gc(self):
         self.dvc("gc", "-f", "-w", proc=True)
@@ -33,7 +31,7 @@ class CloudGCBench(BaseRemoteBench):
 
         # remove everything from the local system
         self.dvc("remove", "data.dvc")
-        self.dvc("gc", "-w")
+        self.dvc("gc", "-w", "-f")
 
     def time_gc_cloud(self, _):
         self.dvc("gc", "-c", "-f", "-w", proc=True)

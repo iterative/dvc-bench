441dc2a5d0b6df63684c2f8fb5f0ffbf8f911b68
Ruslan Kuprieiev <kupruser@gmail.com>
2021-08-12 21:43:23 +0300

    Update update.py

--- benchmarks/update.py
+++ benchmarks/update.py
@@ -5,7 +5,7 @@ class UpdateImportUrlBench(BaseRemoteBench):
     def setup(self, remote):
         super().setup(remote)
         data_url = self.setup_data("100x1024")
-        self.dvc("import-url", data_url, "stage")
+        self.dvc("import-url", data_url, "stage", "-v")
         self.setup_data("200x1024", url=data_url)
 
     def time_import_url_to_remote(self, _):
@@ -17,7 +17,7 @@ class UpdateImportUrlToRemoteBench(BaseRemoteBench):
         super().setup(remote)
         raw_data_url = self.setup_data("100x1024", wrap=False)
         self.dvc(
-            "import-url", self.wrap(raw_data_url), "stage", "--to-remote",
+            "import-url", self.wrap(raw_data_url), "stage", "--to-remote", "-v",
         )
         self.setup_data("200x1024", url=raw_data_url)
 

5be5f1484ffb953c08f5852471fa04e66cd885c0
Ruslan Kuprieiev <kupruser@gmail.com>
2021-09-11 00:03:09 +0300

    benchmarks: diff: fix typo

--- tests/benchmarks/cli/commands/test_diff.py
+++ tests/benchmarks/cli/commands/test_diff.py
@@ -1,3 +1,3 @@
-def test_add(bench_dvc, tmp_dir, scm, dvc, make_dataset):
+def test_diff(bench_dvc, tmp_dir, scm, dvc, make_dataset):
     make_dataset(cache=True, files=True, dvcfile=True, commit=True)
     bench_dvc("diff")

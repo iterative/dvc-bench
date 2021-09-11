b4510a931b64f736c4a5b4bd5ef632b86e5e830d
Ruslan Kuprieiev <kupruser@gmail.com>
2021-09-11 21:57:49 +0300

    benchmarks: diff: add noop test

--- tests/benchmarks/cli/commands/test_diff.py
+++ tests/benchmarks/cli/commands/test_diff.py
@@ -1,3 +1,4 @@
 def test_diff(bench_dvc, tmp_dir, scm, dvc, make_dataset):
     make_dataset(cache=True, files=True, dvcfile=True, commit=True)
     bench_dvc("diff")
+    bench_dvc("diff", name="noop")

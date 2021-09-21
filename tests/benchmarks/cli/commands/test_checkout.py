244a6dc05e0556f996e3dc97bf6c763b4af47eb4
Ruslan Kuprieiev <kupruser@gmail.com>
2021-09-21 21:11:30 +0300

    bench: checkout: add noop and update

--- tests/benchmarks/cli/commands/test_checkout.py
+++ tests/benchmarks/cli/commands/test_checkout.py
@@ -1,3 +1,6 @@
 def test_checkout(bench_dvc, tmp_dir, dvc, make_dataset):
     dataset = make_dataset(dvcfile=True, cache=True, files=False)
     bench_dvc("checkout", dataset)
+    bench_dvc("checkout", name="noop")
+    (dataset / "new").write_text("new")
+    bench_dvc("checkout", "--force", name="update")

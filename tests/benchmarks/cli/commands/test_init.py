9188cdf053abee68e1fe62b5b5f4a1aff1f656fb
Paweł Redzyński <pawelredzynski@gmail.com>
2022-01-11 12:41:20 +0100

    benchmark: cli: init: increase number of rounds (#311)
    Fixes: #310

--- tests/benchmarks/cli/commands/test_init.py
+++ tests/benchmarks/cli/commands/test_init.py
@@ -1,2 +1,12 @@
+import os
+import shutil
+
+
+def remove_dvc_root():
+    dvc_root = ".dvc"
+    if os.path.isdir(dvc_root):
+        shutil.rmtree(".dvc")
+
+
 def test_init(bench_dvc, tmp_dir, scm):
-    bench_dvc("init")
+    bench_dvc("init", setup=remove_dvc_root, rounds=10, warmup_rounds=1)

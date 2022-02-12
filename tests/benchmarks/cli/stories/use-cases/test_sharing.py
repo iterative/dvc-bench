de60f60bb91d786375623e78a0b639314abf48b8
Daniele Trifirò <daniele@iterative.ai>
2022-02-12 16:12:58 +0100

    add benchmark with jobs number

--- tests/benchmarks/cli/stories/use-cases/test_sharing.py
+++ tests/benchmarks/cli/stories/use-cases/test_sharing.py
@@ -1,5 +1,7 @@
 import shutil
 
+import pytest
+
 
 def test_sharing(bench_dvc, tmp_dir, dvc, dataset, remote):
     bench_dvc("add", dataset)
@@ -15,3 +17,17 @@ def test_sharing(bench_dvc, tmp_dir, dvc, dataset, remote):
     bench_dvc("pull", name="noop")
 
     bench_dvc("checkout", name="noop")
+
+
+@pytest.mark.parametrize("jobs", [None, 1, 2, 4])
+def test_jobs(bench_dvc, tmp_dir, dvc, dataset, remote, jobs):
+    if jobs:
+        args = ("-j", str(jobs))
+
+    bench_dvc("add", dataset)
+    bench_dvc("push", *args)
+
+    shutil.rmtree(dataset)
+    shutil.rmtree(dvc.odb.local.cache_dir)
+
+    bench_dvc("pull", *args)

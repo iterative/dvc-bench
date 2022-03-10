b09ab95321ca6245ca322dcbd5db0f1c3c89221a
Daniele Trifirò <daniele@iterative.ai>
2022-03-10 18:18:05 +0100

    DROPME: temporarily use custom repo to test fix

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ set -x
 
 export PIPENV_IGNORE_VIRTUALENVS=1
 
-REVS=( "2.9.3" "2.8.3" "2.7.3" "2.6.3" )
+REVS=("fix/7414-hang-on-pull")
 
 if [ ! -d "dvc" ]; then
-  git clone https://github.com/iterative/dvc
+  git clone https://github.com/dtrifiro/dvc
   python -m venv dvc/.venv
 fi
 
@@ -18,5 +18,5 @@ for REV in ${REVS[@]}; do
   ./.venv/bin/pip install -e '.[all,tests]'
   popd
 
-  pytest --benchmark-save $REV $@ --dvc-bin "$(pwd)/dvc/.venv/bin/dvc"
+  pytest --benchmark-save $(echo $REV | sed 's|/|_|') $@ --dvc-bin "$(pwd)/dvc/.venv/bin/dvc"
 done

2e48d81a88bf620e36d53687dee24c659d586195
Ruslan Kuprieiev <kupruser@gmail.com>
2022-04-05 13:27:10 +0300

    status: modify the dataset

--- tests/benchmarks/cli/commands/test_status.py
+++ tests/benchmarks/cli/commands/test_status.py
@@ -1,4 +1,7 @@
 def test_status(bench_dvc, tmp_dir, dvc, make_dataset):
-    make_dataset(files=True, dvcfile=True, cache=True)
+    dataset = make_dataset(files=True, dvcfile=True, cache=True)
     bench_dvc("status")
-    bench_dvc("status", name="noop")
+
+    (dataset / "new").write_text("new")
+    bench_dvc("status", name="changed")
+    bench_dvc("status", name="changed-noop")

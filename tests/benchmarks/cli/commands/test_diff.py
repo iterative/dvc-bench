10a3dd65d234a15dd9fd468050877c5374521d08
Ruslan Kuprieiev <kupruser@gmail.com>
2022-05-16 00:47:51 +0300

    tests: diff: add changed and changed-noop

--- tests/benchmarks/cli/commands/test_diff.py
+++ tests/benchmarks/cli/commands/test_diff.py
@@ -1,4 +1,8 @@
 def test_diff(bench_dvc, tmp_dir, scm, dvc, make_dataset):
-    make_dataset(cache=True, files=True, dvcfile=True, commit=True)
+    dataset = make_dataset(cache=True, files=True, dvcfile=True, commit=True)
     bench_dvc("diff")
     bench_dvc("diff", name="noop")
+
+    (dataset / "new").write_text("new")
+    bench_dvc("diff", name="changed")
+    bench_dvc("diff", name="changed-noop")

f6f1e682bea6de40f999be8beb2fc38fab37e5bc
Ruslan Kuprieiev <kupruser@gmail.com>
2022-05-30 02:23:01 +0300

    bench: status: add status-noop

--- tests/benchmarks/cli/commands/test_status.py
+++ tests/benchmarks/cli/commands/test_status.py
@@ -1,6 +1,7 @@
 def test_status(bench_dvc, tmp_dir, dvc, make_dataset):
     dataset = make_dataset(files=True, dvcfile=True, cache=True)
     bench_dvc("status")
+    bench_dvc("status", name="noop")
 
     (dataset / "new").write_text("new")
     bench_dvc("status", name="changed")

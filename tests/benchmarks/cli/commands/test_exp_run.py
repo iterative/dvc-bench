9be454b2bced7932772527629a6375ac57f2f4a4
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-10 01:21:09 +0300

    bench: exp: don't forget to pull

--- tests/benchmarks/cli/commands/test_exp_run.py
+++ tests/benchmarks/cli/commands/test_exp_run.py
@@ -1,8 +1,10 @@
-def test_exp_run(make_project, monkeypatch, bench_dvc):
+def test_exp_run(make_project, monkeypatch, dvc_bin, bench_dvc):
     url = "https://github.com/dberenbaum/lstm_seq2seq"
     rev = "dvc"
     path = make_project(url, rev=rev)
     monkeypatch.chdir(path)
+
+    dvc_bin("pull")
 
     bench_dvc("exp", "run")
     bench_dvc("exp", "run", name="noop")

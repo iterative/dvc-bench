714b91e449f6cc209596989e7b60184564359bc4
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-10 00:42:41 +0300

    bench: exp: try using lstm_seq2seq

--- tests/benchmarks/cli/commands/test_exp_show.py
+++ tests/benchmarks/cli/commands/test_exp_show.py
@@ -1,9 +1,14 @@
 import pytest
 
 
-def test_exp_show(project, bench_dvc, dvc_bin):
+def test_exp_show(make_project, monkeypatch, bench_dvc, dvc_bin):
     if dvc_bin.version < (2, 10, 0):
         pytest.skip()
+
+    url = "https://github.com/dberenbaum/lstm_seq2seq"
+    rev = "e24a469cbbf869a2eb0e6e5df57264f05d0ad654"
+    path = make_project(url, rev=rev)
+    monkeypatch.chdir(path)
 
     dvc_bin("exp", "pull", "-A", "--no-cache", "origin")
 

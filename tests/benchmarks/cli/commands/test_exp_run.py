e7549193d507ae438e42fb270d1aff4b10f79958
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-13 19:27:21 +0300

    bench: exp: use example-dvc-experiments

--- tests/benchmarks/cli/commands/test_exp_run.py
+++ tests/benchmarks/cli/commands/test_exp_run.py
@@ -1,6 +1,6 @@
 def test_exp_run(make_project, monkeypatch, dvc_bin, bench_dvc):
-    url = "https://github.com/dberenbaum/lstm_seq2seq"
-    rev = "dvc"
+    url = "https://github.com/iterative/example-dvc-experiments"
+    rev = "main"
     path = make_project(url, rev=rev)
     monkeypatch.chdir(path)
 

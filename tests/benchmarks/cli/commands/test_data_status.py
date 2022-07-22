f2c567d161a1b9eb7bf71632226df03c097132e8
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-07-22 18:24:20 +0545

    add benchmark for data status

---
+++ tests/benchmarks/cli/commands/test_data_status.py
@@ -0,0 +1,19 @@
+import pytest
+from shutil import rmtree
+
+
+def test_data_status(dvc_bin, bench_dvc, tmp_dir, scm, dvc, make_dataset):
+    if dvc_bin.version <= (2, 14, 0):
+        pytest.skip()
+
+    dataset = make_dataset(cache=True, files=True, dvcfile=True, commit=False)
+    rmtree(dvc.tmp_dir)
+
+    bench_dvc("data", "status", name="new")
+    bench_dvc("data", "status", name="noop")
+
+    tmp_dir.scm_add(dataset.with_suffix(".dvc").name, commit="add dataset")
+
+    (dataset / "new").write_text("new")
+    bench_dvc("data", "status", name="changed")
+    bench_dvc("data", "status", name="changed-noop")

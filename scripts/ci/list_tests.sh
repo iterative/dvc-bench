6d1e5daac443632cde177e678e9e8c5e43bbfa51
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-24 22:21:23 +0300

    gha: tests: handle parametrized test listing

--- scripts/ci/list_tests.sh
+++ scripts/ci/list_tests.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-echo $(pytest --collect-only tests/benchmarks -q | grep tests/benchmarks | sed "s/\[None\]//g" | jq -R -s -c 'split("\n")[:-1] | map({path: ., name: . | split(":")[-1]})')
+echo $(pytest --collect-only tests/benchmarks -q | grep tests/benchmarks | sed "s/\[None.*\]//g" | jq -R -s -c 'split("\n")[:-1] | map({path: ., name: . | split(":")[-1]})')

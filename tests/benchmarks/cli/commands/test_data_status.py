707e49a6f7386a7e96e936fef4e6ca01d4953fbc
skshetry <suagatchhetri@outlook.com>
2022-07-25 01:10:05 +0545

    data status: bench with all flags including json (#372)
    * data status: bench with all flags including json
    
    This is what is going to be used in VSCode.
    
    * gha: tests: handle parametrized test listing
    
    Co-authored-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- tests/benchmarks/cli/commands/test_data_status.py
+++ tests/benchmarks/cli/commands/test_data_status.py
@@ -1,19 +1,37 @@
-import pytest
 from shutil import rmtree
 
+import pytest
 
-def test_data_status(dvc_bin, bench_dvc, tmp_dir, scm, dvc, make_dataset):
+
+@pytest.mark.parametrize(
+    "args",
+    (
+        (),
+        (
+            "--granular",
+            "--unchanged",
+            "--untracked-files",
+            "--with-dirs",
+            "--json",
+        ),
+    ),
+    ids=["default", "all_flags"],
+)
+def test_data_status(
+    dvc_bin, bench_dvc, tmp_dir, scm, dvc, make_dataset, args
+):
     if dvc_bin.version < (2, 15, 0):
         pytest.skip()
 
+    args = ("data", "status", *args)
     dataset = make_dataset(cache=True, files=True, dvcfile=True, commit=False)
     rmtree(dvc.tmp_dir)
 
-    bench_dvc("data", "status", name="new")
-    bench_dvc("data", "status", name="noop")
+    bench_dvc(*args, name="new")
+    bench_dvc(*args, name="noop")
 
     tmp_dir.scm_add(dataset.with_suffix(".dvc").name, commit="add dataset")
 
     (dataset / "new").write_text("new")
-    bench_dvc("data", "status", name="changed")
-    bench_dvc("data", "status", name="changed-noop")
+    bench_dvc(*args, name="changed")
+    bench_dvc(*args, name="changed-noop")

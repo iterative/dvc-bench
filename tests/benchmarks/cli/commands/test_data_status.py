320c42d9afe6a055f556b0d08cf1895d3449fa96
Ruslan Kuprieiev <kupruser@gmail.com>
2022-08-03 00:33:10 +0300

    bench: test_data_status: temporary split into two
    Temporary workaround for #373

--- tests/benchmarks/cli/commands/test_data_status.py
+++ tests/benchmarks/cli/commands/test_data_status.py
@@ -3,27 +3,46 @@ from shutil import rmtree
 import pytest
 
 
-@pytest.mark.parametrize(
-    "args",
-    (
-        (),
-        (
-            "--granular",
-            "--unchanged",
-            "--untracked-files",
-            "--with-dirs",
-            "--json",
-        ),
-    ),
-    ids=["default", "all_flags"],
-)
 def test_data_status(
-    dvc_bin, bench_dvc, tmp_dir, scm, dvc, make_dataset, args
+    dvc_bin,
+    bench_dvc,
+    tmp_dir,
+    scm,
+    dvc,
+    make_dataset,
 ):
     if dvc_bin.version < (2, 15, 0):
         pytest.skip()
 
-    args = ("data", "status", *args)
+    args = ("data", "status")
+    dataset = make_dataset(cache=True, files=True, dvcfile=True, commit=False)
+    rmtree(dvc.tmp_dir)
+
+    bench_dvc(*args, name="new")
+    bench_dvc(*args, name="noop")
+
+    tmp_dir.scm_add(dataset.with_suffix(".dvc").name, commit="add dataset")
+
+    (dataset / "new").write_text("new")
+    bench_dvc(*args, name="changed")
+    bench_dvc(*args, name="changed-noop")
+
+
+def test_data_status_all_flags(
+    dvc_bin, bench_dvc, tmp_dir, scm, dvc, make_dataset
+):
+    if dvc_bin.version < (2, 15, 0):
+        pytest.skip()
+
+    args = (
+        "data",
+        "status",
+        "--granular",
+        "--unchanged",
+        "--untracked-files",
+        "--with-dirs",
+        "--json",
+    )
     dataset = make_dataset(cache=True, files=True, dvcfile=True, commit=False)
     rmtree(dvc.tmp_dir)
 

beb51b0a73f181d27b2a65475d83dfc394206c93
Peter Rowlands <peter@pmrowla.com>
2023-04-18 18:11:16 +0900

    use-cases: test against all enabled remotes in test_sharing

--- tests/benchmarks/cli/stories/use-cases/test_sharing.py
+++ tests/benchmarks/cli/stories/use-cases/test_sharing.py
@@ -1,5 +1,12 @@
 import shutil
 
+def pytest_generate_tests(metafunc):
+    if "remote" in metafunc.fixturenames:
+        config = metafunc.config.dvc_config
+        remotes = set(config.enabled_remotes)
+        remotes.add(config.remote)
+        metafunc.parametrize("remote", remotes, indirect=True)
+
 
 def test_sharing(bench_dvc, tmp_dir, dvc, dataset, remote):
     bench_dvc("add", dataset)

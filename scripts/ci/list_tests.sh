77b87b4fdf2f5a73aa4716b617e4a1018ece39c4
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2023-05-09 11:11:13 +0545

    test virtual operation

--- scripts/ci/list_tests.sh
+++ scripts/ci/list_tests.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-pytest --collect-only -q --pyargs dvc.testing.benchmarks | grep test_ | sed -E 's/([A-Za-z\/_]+)(\.py)(::([A-Za-z0-9_]+))?.*/dvc.testing.benchmarks.\1\3 \4/' | sed 's/\//./g' | jq -R -s -c 'split("\n")[:-1] | map({path: . | split(" ")[0], name: . | split(" ")[-1]})'
+pytest --collect-only -q --pyargs dvc.testing.benchmarks | grep -E '(sharing|add)' | sed -E 's/([A-Za-z\/_]+)(\.py)(::([A-Za-z0-9_]+))?.*/dvc.testing.benchmarks.\1\3 \4/' | sed 's/\//./g' | jq -R -s -c 'split("\n")[:-1] | map({path: . | split(" ")[0], name: . | split(" ")[-1]})'

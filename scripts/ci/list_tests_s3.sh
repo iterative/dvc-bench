45feb8c04f0900a533a146792cff40d1f555567d
skshetry <18718008+skshetry@users.noreply.github.com>
2023-05-09 13:55:01 +0545

    Update list_tests_s3.sh

--- scripts/ci/list_tests_s3.sh
+++ scripts/ci/list_tests_s3.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-pytest --collect-only -q --pyargs dvc_s3.tests.benchmarks | grep test_ | sed -E 's/::([A-Za-z0-9_]+).*/dvc_s3.tests.benchmarks::\1 \1/' | sed 's/\//./g' | jq -R -s -c 'split("\n")[:-1] | map({path: . | split(" ")[0], name: . | split(" ")[-1]})'
+pytest --collect-only -q --pyargs dvc_s3.tests.benchmarks | grep test_notexistingtests | sed -E 's/::([A-Za-z0-9_]+).*/dvc_s3.tests.benchmarks::\1 \1/' | sed 's/\//./g' | jq -R -s -c 'split("\n")[:-1] | map({path: . | split(" ")[0], name: . | split(" ")[-1]})'

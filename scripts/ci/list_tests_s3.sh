67d3bb5befd7e5f66661d59d9a736b30ab1d15b8
Peter Rowlands (변기호) <peter@pmrowla.com>
2023-05-19 11:51:14 +0900

    ci: run azure benchmarks (#456)
    * ci: run azure benchmarks
    
    * drop 2.18 and 2.45 from cloud benchmarks to reduce runtime

--- scripts/ci/list_tests_s3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-pytest --collect-only -q --pyargs dvc_s3.tests.benchmarks | grep test_ | sed -E 's/::([A-Za-z0-9_]+).*/dvc_s3.tests.benchmarks::\1 \1/' | sed 's/\//./g' | jq -R -s -c 'split("\n")[:-1] | map({path: . | split(" ")[0], name: . | split(" ")[-1]})'

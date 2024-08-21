41a11bb9def6c1894663e0e63e0255d9a8b2d35a
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2024-08-21 22:26:56 +0545

    make it work for all revisions

---
+++ scripts/ci/fetch-all-revs.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Create local branch for all revs except the current branch and tags
+# All the branches are assumed to have been fetched from the remote already.
+IFS=',' read -r -a branch_array <<< "$1"
+for branch in "${branch_array[@]}"; do
+  if ! git rev-parse --verify $branch &>/dev/null; then
+    git branch "${branch}" origin/"${branch}"
+  fi
+done

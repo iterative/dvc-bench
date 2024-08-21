43ce3fc672c32264d7a8f2c44c11adef181629f1
skshetry <18718008+skshetry@users.noreply.github.com>
2024-08-21 21:42:44 +0545

    run benchmarks from individual repositories (#536)

--- scripts/ci/list_tests.sh
+++ scripts/ci/list_tests.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-pytest --collect-only -q --pyargs dvc.testing.benchmarks | grep test_ | sed -E 's/([A-Za-z\/_]+)(\.py)(::([A-Za-z0-9_]+))?.*/dvc.testing.benchmarks.\1\3 \4/' | sed 's/\//./g' | jq -R -s -c 'split("\n")[:-1] | map({path: . | split(" ")[0], name: . | split(" ")[-1]})'
+pytest --collect-only -q $1 | head -n -2 | sed 's/\[[^]]*\]//g' | jq -Rcs 'split("\n")[:-1] | map(. as $p | split("::") | {path: $p, name: .[1]})'
--- scripts/ci/list_tests_cloud.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-PLUGIN="dvc_$1"
-pytest --collect-only -q --pyargs "$PLUGIN.tests.benchmarks" | grep test_ | sed -E "s/::([A-Za-z0-9_]+).*/$PLUGIN.tests.benchmarks::\\1 \\1/" | sed 's/\//./g' | jq -R -s -c 'split("\n")[:-1] | map({path: . | split(" ")[0], name: . | split(" ")[-1]})'

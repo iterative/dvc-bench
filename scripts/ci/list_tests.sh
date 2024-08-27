8ca774ad38a7c94633a78fa219bf16bf45a2e0c7
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2024-08-27 16:21:53 +0545

    allow cprofiling and running tests selectively

--- scripts/ci/list_tests.sh
+++ scripts/ci/list_tests.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-pytest --collect-only -q $1 | head -n -2 | sed 's/\[[^]]*\]//g' | jq -Rcs 'split("\n")[:-1] | map(. as $p | split("::") | {path: $p, name: .[1]})'
+pytest --collect-only -q $1 | grep test_ | sed 's/\[[^]]*\]//g' | jq -Rcs 'split("\n")[:-1] | map(. as $p | split("::") | {path: $p, name: .[1]})' | jq -c --arg names "$2" '
+if ($names | length > 0) then
+  ($names | split(",")) as $_names | map(select(.name | IN($_names[])))
+else
+  .
+end
+'

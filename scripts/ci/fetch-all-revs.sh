96d541f24dcfa94e5988a2019440eeb0bcd0ea28
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-07-08 11:03:02 +0545

    support per-test additional revs

--- scripts/ci/fetch-all-revs.sh
+++ scripts/ci/fetch-all-revs.sh
@@ -4,6 +4,7 @@
 # All the branches are assumed to have been fetched from the remote already.
 IFS=',' read -r -a branch_array <<< "$1"
 for branch in "${branch_array[@]}"; do
+  git fetch origin $branch
   if ! git rev-parse --verify $branch &>/dev/null; then
     git branch "${branch}" origin/"${branch}"
   fi
